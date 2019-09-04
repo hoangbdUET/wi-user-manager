e71f34635ad0ac1b3250ce5de7cac3ffb976a1ef
Hoang Dang <bdhoang.ht@gmail.com>
2019-09-04 17:38:03 +0700

    user

--- src/pages/LeftNavigation/index.js
+++ src/pages/LeftNavigation/index.js
@@ -1,24 +1,25 @@
 module.exports = LeftNavigation;
 require('./style.less');
-const Link = require('react-router-dom').Link;
+const NavLink = require('react-router-dom').NavLink;
 const React = require('react');
 
 function LeftNavigation(props) {
-	let routes = props.routes;
-	let classMap = {
-		'Group': 'tab-icon ti ti-basketball',
-		'Company': 'tab-icon ti ti-basketball active-tab',
-		'User': 'tab-icon ti ti-basketball',
-		'Project': 'tab-icon ti ti-basketball'
-	}
-	return (<div className={"LeftNavigation"}>
-		<div className="logo">
+    let routes = props.routes;
+    let classMap = {
+        'Group': 'tab-icon ti ti-basketball',
+        'Company': 'tab-icon ti ti-basketball',
+        'User': 'tab-icon ti ti-basketball',
+        'Project': 'tab-icon ti ti-basketball'
+    };
+    return (<div className={"LeftNavigation"}>
+        <div className="logo">
 
-		</div>
-		{routes.map((aRoute, idx) => 
-			<div key={idx}>
-				<Link to={aRoute.path}>{<div className={classMap[aRoute.label]}></div>}</Link>
-			</div>
-			)}
-	</div>)
+        </div>
+        {routes.map((aRoute, idx) =>
+            <div key={idx}>
+                <NavLink className={classMap[aRoute.label]} to={aRoute.path}
+                         activeStyle={{background: '#4B7DEF', color: '#fff'}}>{aRoute.label}</NavLink>
+            </div>
+        )}
+    </div>)
 }
