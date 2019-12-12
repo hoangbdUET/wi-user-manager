c46105af7c3757a0c64950c7ee3e5fab2a7509b3
Nam Phan <sockyone>
2019-12-12 17:04:29 +0700

    hot fix

--- src/components/MyFilteredList/index.js
+++ src/components/MyFilteredList/index.js
@@ -1,12 +1,5 @@
 const React = require('react');
 
 function MyFilteredList(props) {
-    let Component = props.Component;
-    let searchStr = props.searchStr;
-    let companyFilter = props.companyFilter;
-    
-    delete props.searchStr;
-    delete props.companyFilter;
-    delete props.Component;
-    return <Component {...props}/>
+    return <div></div>
 }
