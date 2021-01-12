a00fee29f66fff234aaecfa2721f84aca6ee505b
Hoang Bui <bdhoang.ht@gmail.com>
2021-01-12 16:56:47 +0700

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-git stash
 git pull
-git stash pop
 npm i
 npx webpack --mode=production
-docker rmi registry.i2g.cloud/pvep/wi-user-manager
-docker build -t registry.i2g.cloud/pvep/wi-user-manager .
-docker push registry.i2g.cloud/pvep/wi-user-manager
-kubectl patch deployment wi-user-manager-client -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
+docker rmi revotech2017/wi-user-manager:hlhvjoc
+docker build -t revotech2017/wi-user-manager:hlhvjoc .
+docker push revotech2017/wi-user-manager:hlhvjoc
