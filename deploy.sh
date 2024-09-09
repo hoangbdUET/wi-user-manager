9bfb4d1f71808b3102f0ad8112280558b8d63377
Revotech <jira.i2g.cloud@gmail.com>
2024-09-09 16:21:26 +0700

    update

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git pull
-npm i
-npx webpack --mode=production
-docker rmi revotech2017/wi-user-manager:hlhvjoc
-docker build -t revotech2017/wi-user-manager:hlhvjoc .
-docker push revotech2017/wi-user-manager:hlhvjoc
